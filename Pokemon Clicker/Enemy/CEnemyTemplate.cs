e6f2e9e6efd18ef0251e7badb59dbff295ac834d
Kamys <work.kamysdev@gmail.com>
2024-09-06 11:25:34 +0700

    Created base Enemy class

---
+++ Pokemon Clicker/Enemy/CEnemyTemplate.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pokemon_Clicker.Enemy
+{
+    public class CEnemyTemplate
+    {
+        private string name;
+        private string iconName;
+
+        private int baseLife;
+        private double lifeModifier;
+
+        private int baseGold;
+        private double goldModifier;
+
+        private double spawnChance;
+
+        public CEnemyTemplate(string name, string iconName, int baseLife, double lifeModifier, int baseGold, double goldModifier, double spawnChance)
+        {
+            this.name = name;
+            this.iconName = iconName;
+            this.baseLife = baseLife;
+            this.lifeModifier = lifeModifier;
+            this.baseGold = baseGold;
+            this.goldModifier = goldModifier;
+            this.spawnChance = spawnChance;
+        }
+
+        public string GetName() { return name; }
+        public string GetIcon() { return iconName; }
+        public int GetBaseLife() { return baseLife; } 
+        public double GetLifeModifier() { return lifeModifier; }
+        public int GetGold() {  return baseGold; }
+        public double GetGoldModifier() { return goldModifier; }
+        public double GetSpawnChance() { return spawnChance; }
+    }
+}
---
+++ Pokemon Clicker/Enemy/CEnemyTemplateList.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Pokemon_Clicker.Enemy
+{
+    public class CEnemyTemplateList
+    {
+        List<CEnemyTemplate> enemies;
+
+        public CEnemyTemplateList()
+        {
+            enemies = new List<CEnemyTemplate>();
+        }
+
+        public void AddEnemy(CEnemyTemplate enemy)
+        {
+            enemies.Add(enemy);
+        }
+
+        public void SaveToJson(string path)
+        {
+
+        }
+    }
+
+    public class CEnemytemplateListJson 
+    {
+        [JsonInclude]
+        public List<CEnemyTemplate> enemies;
+    }
+}
