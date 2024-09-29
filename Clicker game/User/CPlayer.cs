e295d3c3ab3de3a203db571036d8cceb7dfbe4ab
Kamys <work.kamysdev@gmail.com>
2024-09-29 17:37:23 +0700

    Created base CPlayer class

---
+++ Clicker game/User/CPlayer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Clicker_game.Data;
+
+namespace Clicker_game.User
+{
+    public class CPlayer
+    {
+        private int lvl;
+
+        private BigNumber gold;
+
+        private BigNumber damage;
+
+        private double damageModifier;
+
+        private BigNumber upgradeCost;
+
+        private double upgradeCostModifier;
+
+        public CPlayer(BigNumber gold, BigNumber damage,
+            double damageModifier, BigNumber upgradeCost, double upgradeCostModifier)
+        {
+            this.gold = gold;
+            this.damage = damage;
+            this.damageModifier = damageModifier;
+            this.upgradeCost = upgradeCost;
+            this.upgradeCostModifier = upgradeCostModifier;
+        }
+
+    }
+}
